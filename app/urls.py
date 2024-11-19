c80ab57fb897ed80398070f8d709d508a8aea263
Fleshspeed <bexruzravshanov506@gmail.com>
2024-11-19 19:35:41 +0500

    commit

--- app/urls.py
+++ app/urls.py
@@ -1,26 +1,21 @@
-from .views import BookListView, BookDetailView,BookDeleteView,BookUpdateView
+from .views import BookListView, BookDetailView, BookDeleteView, BookUpdateView
 from django.urls import path, include
 from .views import EmployeeViewSet
 from rest_framework.routers import DefaultRouter
 from . import views
+
 router = DefaultRouter()
-router.register(r'employees', EmployeeViewSet)
+router.register(r'book', EmployeeViewSet, basename='user')
+
+
 urlpatterns = [
-    path('api/book/', BookListView.as_view(), name='book_list'),
+    path('api/book/get', BookListView.as_view(), name='book_list'),
     path('api/book/<int:book_id>/', BookDetailView.as_view(), name='book_detail'),
     path('api/book/delete/<int:book_id>/', BookDeleteView.as_view(), name='book_delete'),
     path('api/book/update/<int:book_id>/', BookUpdateView.as_view(), name='book_update'),
-    path('list/', views.list, name='list'),
+    path('', views.list, name='list'),
     path('create/', views.create, name='create'),
     path('<int:pk>/update/', views.update, name='update'),
     path('<int:pk>/delete/', views.delete, name='delete'),
     path('api/', include(router.urls)),
-
-
-    
 ]
-
-
-
-
-
